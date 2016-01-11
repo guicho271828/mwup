de9fe0146a08cc3a3be46fa102193af7e285b196
Masataro Asai <guicho2.71828@gmail.com>
2016-01-11 21:58:47 +0900

    implemented benchmark

--- t/junk-generation-benchmark.lisp
+++ t/junk-generation-benchmark.lisp
@@ -4,13 +4,18 @@
 (def-suite :mwup.benchmark)
 (in-suite :mwup.benchmark)
 
+(defun rel (path)
+  (asdf:system-relative-pathname :mwup path))
+
 (test benchmark
       ;; expect >4000 macros
-  (multiple-value-bind (dname domain) (suppress (parse-file "t/test1/domain.pddl" nil t))
-    (multiple-value-bind (pname problem) (suppress (parse-file "t/test1/p01.pddl" nil t))
+  (multiple-value-bind (dname domain) (mwup::suppress (parse-file (rel "t/test1/domain.pddl") nil t))
+    (multiple-value-bind (pname problem) (mwup::suppress (parse-file (rel "t/test1/p01.pddl") nil t))
       (let ((length 3)
             (quantity 100))
-        (time (junk-macros length quantity (get-all-ground-actions domain problem) domain problem))
+        (time (let ((mwup::*start* (get-universal-time)))
+                (mwup::suppress
+                  (mwup::junk-macros length quantity (mwup::get-all-ground-actions domain problem) domain problem))))
         ;; (junk-macros length
         ;;              quantity
         ;;              (get-all-ground-actions domain problem)
@@ -20,3 +25,6 @@
         ;;              (get-all-ground-actions domain problem)
         ;;              domain problem)
         ))))
+
+#+nil
+(5am:run! 'mwup.test::benchmark)
