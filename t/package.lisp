6362003cbac082f534da2bd8721ffcd28bc41af2
Masataro Asai <guicho2.71828@gmail.com>
2016-01-17 18:28:33 +0900

    reduce test output, but runs validation

--- t/package.lisp
+++ t/package.lisp
@@ -31,9 +31,8 @@
                       (mapcar #'namestring (flatten args)))))
     (format t "~&~a~%" cmd)
     (run-program cmd
-                 :output t
-                 :error-output t
-                 :force-shell t)))
+                 :output *standard-output*
+                 :error-output *error-output*)))
 
 (test ros-dry-runs
   (finishes
@@ -41,33 +40,31 @@
   (finishes
     (launch "--plain"))
   (finishes
-    (launch "--enhance-only"))
-  (finishes
-    (launch "-v" "--plain")))
+    (launch "--enhance-only")))
 
 (test plain
   (let ((*default-pathname-defaults*
          (asdf:system-source-directory :mwup)))
     (finishes
-      (launch "-v" "--plain" "t/test1/p01.pddl"))
+      (launch "--validation" "--plain" "t/test1/p01.pddl"))
     (finishes
-      (launch "-v" "--plain" "t/test2/p01.pddl" "t/test2/domain.pddl"))
+      (launch "--validation" "--plain" "t/test2/p01.pddl" "t/test2/domain.pddl"))
     (finishes
-      (launch "-v" "--plain" "t/test3/p01.pddl" "t/test3/domain.pddl"
+      (launch "--validation" "--plain" "t/test3/p01.pddl" "t/test3/domain.pddl"
               (directory (merge-pathnames "t/test3/p01.macro.*"))))))
 
 (test macros
   (let ((*default-pathname-defaults*
          (asdf:system-source-directory :mwup)))
     (finishes
-      (launch "-v" "--validation" "t/test1/p01.pddl"))
+      (launch "--validation" "t/test1/p01.pddl"))
     (finishes
-      (launch "-v" "--validation" "t/test1/p01.pddl" "t/test1/domain.pddl"))
+      (launch "--validation" "t/test1/p01.pddl" "t/test1/domain.pddl"))
     (finishes
-      (launch "-v" "--validation" "t/test1/p01.pddl" "t/test1/domain.pddl"
+      (launch "--validation" "t/test1/p01.pddl" "t/test1/domain.pddl"
               (directory (merge-pathnames "t/test1/p01.macro.*"))))
     (finishes
-      (launch "-v" "--validation" "--add-macro-cost"
+      (launch "--validation" "--add-macro-cost"
               "t/test1/p01.pddl" "t/test1/domain.pddl"
               (directory (merge-pathnames "t/test1/p01.macro.*"))))))
 
@@ -77,66 +74,66 @@
     ;; does not work due to cgroup
     ;; (finishes
     ;;   (let (*verbose* *junk*)
-    ;;     (mwup::main "-v" "--junk" "2" "10" "t/test3/p01.pddl" "t/test3/domain.pddl")))
+    ;;     (mwup::main "--junk" "2" "10" "t/test3/p01.pddl" "t/test3/domain.pddl")))
     (finishes
-      (launch "-v" "--validation" "--junk" "2" "10" "t/test3/p01.pddl" "t/test3/domain.pddl"))
+      (launch "--validation" "--junk" "2" "10" "t/test3/p01.pddl" "t/test3/domain.pddl"))
     (signals error
       ;; old arguments: expect probability
-      (launch "-v" "--validation" "--junk" "2" "0.5" "t/test3/p01.pddl" "t/test3/domain.pddl"))))
+      (launch "--validation" "--junk" "2" "0.5" "t/test3/p01.pddl" "t/test3/domain.pddl"))))
 
 (test junk-limit
   (let ((*default-pathname-defaults*
          (asdf:system-source-directory :mwup)))
     (signals error
       ;; old arguments
-      (launch "-v" "--junk" "2" "10" "--junk-limit" "5" "t/test3/p01.pddl" "t/test3/domain.pddl"))
+      (launch "--junk" "2" "10" "--junk-limit" "5" "t/test3/p01.pddl" "t/test3/domain.pddl"))
     (signals error
-      (launch "-v" "--junk" "2" "10" "--junk-limit" "5000" "t/test3/p01.pddl" "t/test3/domain.pddl"))
+      (launch "--junk" "2" "10" "--junk-limit" "5000" "t/test3/p01.pddl" "t/test3/domain.pddl"))
     (finishes
-     (launch "-v" "--validation" "--junk" "2" "10" "t/test3/p01.pddl" "t/test3/domain.pddl"))
+     (launch "--validation" "--junk" "2" "10" "t/test3/p01.pddl" "t/test3/domain.pddl"))
     (finishes
-     (launch "-v" "--validation" "--junk" "2" "5000" "t/test3/p01.pddl" "t/test3/domain.pddl"))))
+     (launch "--validation" "--junk" "2" "5000" "t/test3/p01.pddl" "t/test3/domain.pddl"))))
 
 (test gc
   (let ((*default-pathname-defaults*
          (asdf:system-source-directory :mwup)))
     (finishes
-     (launch "-v" "--validation" "--junk" "2" "10" "--megabytes-consed-between-gcs" "10" "t/test3/p01.pddl" "t/test3/domain.pddl"))))
+     (launch "--validation" "--junk" "2" "10" "--megabytes-consed-between-gcs" "10" "t/test3/p01.pddl" "t/test3/domain.pddl"))))
 
 
 (test fastjunk
   (let ((*default-pathname-defaults*
          (asdf:system-source-directory :mwup)))
     (finishes
-     (launch "-v" "--validation" "--fastjunk" "2" "10" "t/test3/p01.pddl" "t/test3/domain.pddl"))
+     (launch "--validation" "--fastjunk" "2" "10" "t/test3/p01.pddl" "t/test3/domain.pddl"))
     (finishes
-     (launch "-v" "--validation" "--fastjunk" "2" "5000" "t/test2/p01.pddl" "t/test2/domain.pddl"))))
+     (launch "--validation" "--fastjunk" "2" "5000" "t/test2/p01.pddl" "t/test2/domain.pddl"))))
 
 (test alljunk
   (let ((*default-pathname-defaults*
          (asdf:system-source-directory :mwup)))
     (finishes
-     (launch "-v"  "--junk" "2" ":infinity" "t/test2/p01.pddl" "t/test2/domain.pddl"))
+     (launch "--validation" "--junk" "2" ":infinity" "t/test2/p01.pddl" "t/test2/domain.pddl"))
     (finishes
-     (launch "-v"  "--fastjunk" "2" ":infinity" "t/test2/p01.pddl" "t/test2/domain.pddl"))
+     (launch "--validation" "--fastjunk" "2" ":infinity" "t/test2/p01.pddl" "t/test2/domain.pddl"))
     (signals error
-     (launch "-v"  "--junk" "2" ":someother" "t/test2/p01.pddl" "t/test2/domain.pddl"))
+     (launch "--junk" "2" ":someother" "t/test2/p01.pddl" "t/test2/domain.pddl"))
     (signals error
-     (launch "-v"  "--fastjunk" "2" ":someother" "t/test2/p01.pddl" "t/test2/domain.pddl"))))
+     (launch "--fastjunk" "2" ":someother" "t/test2/p01.pddl" "t/test2/domain.pddl"))))
 
 (test seed
   (let ((*default-pathname-defaults*
          (asdf:system-source-directory :mwup)))
     (finishes
-     (launch "-v" "--seed" "2016" "--junk" "2" "10" "t/test2/p01.pddl" "t/test2/domain.pddl"))
+     (launch "--validation" "--seed" "2016" "--junk" "2" "10" "t/test2/p01.pddl" "t/test2/domain.pddl"))
     (finishes
-     (launch "-v" "--seed" "t" "--junk" "2" "10" "t/test2/p01.pddl" "t/test2/domain.pddl"))))
+     (launch "--validation" "--seed" "t" "--junk" "2" "10" "t/test2/p01.pddl" "t/test2/domain.pddl"))))
 
 (test junk-type
   (let ((*default-pathname-defaults*
          (asdf:system-source-directory :mwup)))
     (finishes
-     (launch "-v" "--junk" "2" "10" "--junk-type" ":greedy" "t/test2/p01.pddl" "t/test2/domain.pddl"))
+     (launch "--validation" "--junk" "2" "10" "--junk-type" ":greedy" "t/test2/p01.pddl" "t/test2/domain.pddl"))
     (finishes
      (launch "-v" "--junk" "2" "10" "--junk-type" ":reservoir" "t/test2/p01.pddl" "t/test2/domain.pddl"))
     (finishes
