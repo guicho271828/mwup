01adab434a19f76cdcae982a69ad8f4d2f18fc52
Masataro Asai <guicho2.71828@gmail.com>
2016-01-23 12:59:32 +0900

    moved mangling code

--- src/2.plan-plain.lisp
+++ src/2.plan-plain.lisp
@@ -7,35 +7,6 @@
     (uiop:run-program command))
   (namestring dest))
 
-(defgeneric mangle (object))
-(defgeneric demangle (object))
-
-(defvar *demangle* (make-hash-table))
-
-(defmethod mangle :around (object)
-  (let ((result (call-next-method)))
-    (setf (gethash result *demangle*) object)
-    result))
-
-(defmethod mangle ((domain pddl-domain))
-  (let ((*domain* (shallow-copy domain)))
-    (setf (actions *domain*)
-          (mapcar #'mangle (shuffle (actions *domain*))))
-    *domain*))
-
-(defmethod mangle ((a pddl-action))
-  (shallow-copy a
-                :name (gensym "MANGLED")
-                :domain *domain*))
-
-(defmethod demangle (object)
-  (gethash object *demangle* object))
-
-(defmethod demangle ((ga pddl-ground-action))
-  (shallow-copy ga
-                :name (name (demangle (action (domain ga) (name ga))))
-                :domain (demangle (domain ga))))
-
 (defun plan-plain (dpath ppath)
   (let ((dir (mktemp "plain")))
     (handler-bind ((trivial-signal:unix-signal
